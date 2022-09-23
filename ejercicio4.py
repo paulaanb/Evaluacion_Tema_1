2995b018901c2bc1b937173442b487d8f975903a
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-09-23 09:17:52 +0200

    Ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,2 +1,19 @@
 import sys
 print(sys.argv)
+
+if len(sys.argv) == 3:
+    filas = int(sys.argv[1])
+    columnas = int(sys.argv[2])
+
+    if filas < 1 or filas > 9 or columnas < 1 or columnas > 9:
+        print("Error - Filas o columnas incorrectos")
+        print("Ejemplo: tabla.py [1-9] [1-9]")
+    else:
+        for f in range(filas):
+            print("")
+            for c in range(columnas):
+                print(" * ", end='')
+
+else:
+    print("Error - Argumentos incorrectos")
+    print("Ejemplo: tabla.py [1-9] [1-9]")
