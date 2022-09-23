5ab5eead1a638e6ca67b67122add3ac48aab82e9
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-09-23 09:39:16 +0200

    Mejora ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,8 @@
 cadena = input("Escribe una cadena de texto: ")
 print("¿La longitud de la cadena es mayor o igual que 3 y menor que 10?",
       len(cadena) >= 3 and len(cadena) < 10 )
+      if len(cadena) >= 3 and len(cadena) < 10:
+        return True
+      else:
+        return False
+          
